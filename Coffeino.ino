14fa4f7b1a8787ce88e0716da261b8ab471b9ee0
Pavel <paulsedeen@gmail.com>
2016-02-20 21:02:46 +0200

    Reworked
    for arduino nano board.

--- Coffeino.ino
+++ Coffeino.ino
@@ -1,86 +1,33 @@
-// include the library code:
-#include <LiquidCrystal.h>
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int shotSize = 30;
-int pumpRelay = 22;
-int heaterRelay = 24;
+int pumpRelay = 4;
+int heaterRelay = 2;
 int thermalResistor = 7;
+int brewButton = 3;
 
-void setup() {
+void setup() 
+{
   pinMode(pumpRelay, OUTPUT);
   pinMode(heaterRelay, OUTPUT);
   pinMode(thermalResistor, INPUT);
   
   digitalWrite(pumpRelay, HIGH);
   digitalWrite(heaterRelay, HIGH);
-  
-  lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print("    SHOT SIZE");
-  
+
+  attachInterrupt(1, brew, HIGH);
+    
   Serial.begin(9600);
 }
 
-void loop() {
-  readKeys();
-  String shotSizeString = String(shotSize);
-  int shotSizeStringSize = shotSizeString.length();
-  int cursorPosition = (9 - shotSizeStringSize);
-  lcd.setCursor(cursorPosition,1);
-  lcd.print(shotSize);
-}
-
-void readKeys() {
-    int x;
-    x = analogRead(0);
-    if (x < 60) {
-    }
-    else if (x < 200) {
-      
-      if (shotSize < 200) 
-      {
-        shotSize = shotSize + 10;
-        delay(100);
-        clearLine(1);
-      }
-      else
-      {
-        shotSize = 200;        
-      }
-    }
-    else if (x < 400){
-      if (shotSize > 20) 
-      {
-        shotSize = shotSize - 10;
-        delay(100);
-        clearLine(1);
-      }
-      else
-      {
-        shotSize = 20;
-      }
-    }
-    else if (x < 600){
-      warmUp();
-    }
-    else if (x < 800){
-      brew();
-    }
+void loop() 
+{
+  int temp = analogRead(thermalResistor);
 }
 
 void brew()
 {
-  clearScreen();
-  lcd.setCursor(0,0);
-  lcd.print ("    BREWING...");
+  Serial.println("BREWING");
   digitalWrite(pumpRelay, LOW);
-  delay(shotSize * 100);
+  delay(100);
   digitalWrite(pumpRelay, HIGH);
-  clearLine(0);
-  lcd.setCursor(0,0);
-  lcd.print("    SHOT SIZE");
 }
 
 void warmUp()
@@ -99,14 +46,4 @@ void warmUp()
   digitalWrite(heaterRelay, HIGH);
 }
 
-void clearScreen() 
-{
-  clearLine(0);
-  clearLine(1);
-}
 
-void clearLine(int lineNumber)
-{
-  lcd.setCursor(0, lineNumber);
-  lcd.print("                ");
-}
